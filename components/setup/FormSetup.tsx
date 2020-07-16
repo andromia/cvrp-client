09f7fa42fe5b7c9dff8b7eada5fce69a9a4da167
chris pryer <christophpryer@gmail.com>
2020-07-15 22:03:54 -0400

    added demand file parsing

--- components/setup/FormSetup.tsx
+++ components/setup/FormSetup.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Papa from "papaparse";
 
 // Bootstrap
 import Card from "react-bootstrap/Card";
@@ -13,56 +14,66 @@ import Button from "react-bootstrap/Button";
 const FormSetup = () => {
     const [fileName, setFileName] = useState("File Input");
 
-    const onFileSubmit = e => {
-        const newFileName = e.target.value.split("\\").splice(-1)[0];
+    const onFileSubmit = event => {
+        const newFileName = event.target.value.split("\\").splice(-1)[0];
         setFileName(newFileName);
+    };
+
+    const onCreateSubmit = event => {
+        event.preventDefault();
+
+        Papa.parse(event.target[2].files[0], {
+            header: true,
+            complete: function(results) {
+                console.log(results);
+            }
+        });
     };
 
     return (
         <Card>
             <Card.Body>
-                <Row className="mb-4">
-                    <Col lg="6">
-                        <Row className="d-flex flex-column">
-                            <Col className="pb-3">
-                                <div>Origin</div>
-                            </Col>
-                            <Col>
-                                <Row>
-                                    <Col>
-                                        <FormControl className="d-inline-flex" placeholder="Lat." aria-label="Lat." />
-                                    </Col>
-                                    <Col>
-                                        <FormControl className="d-inline-flex" placeholder="Lon." aria-label="Lon." />
-                                    </Col>
-                                </Row>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col lg="6">
-                        <Row className="d-flex flex-column">
-                            <Col className="pb-3">
-                                <div>Demand</div>
-                            </Col>
-                            <Col>
-                                <Form.File id="custom-file" label={fileName} custom onChange={onFileSubmit} />
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row className="mb-4">
-                    <Col className="p-0">
-                        <GoogleMaps />
-                    </Col>
-                </Row>
-                <Row className="d-flex justify-content-end">
-                    <Col lg="auto">
-                        <Button>Check</Button>
-                    </Col>
-                    <Col lg="auto">
-                        <Button>Create</Button>
-                    </Col>
-                </Row>
+                <Form onSubmit={onCreateSubmit}>
+                    <Row className="mb-4">
+                        <Col lg="6">
+                            <Row className="d-flex flex-column">
+                                <Col className="pb-3">
+                                    <div>Origin</div>
+                                </Col>
+                                <Col>
+                                    <Row>
+                                        <Col>
+                                            <FormControl className="d-inline-flex" placeholder="Lat." aria-label="Lat." />
+                                        </Col>
+                                        <Col>
+                                            <FormControl className="d-inline-flex" placeholder="Lon." aria-label="Lon." />
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col lg="6">
+                            <Row className="d-flex flex-column">
+                                <Col className="pb-3">
+                                    <div>Demand</div>
+                                </Col>
+                                <Col>
+                                    <Form.File id="custom-file" label={fileName} custom onChange={onFileSubmit} />
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row className="mb-4">
+                        <Col className="p-0">
+                            <GoogleMaps />
+                        </Col>
+                    </Row>
+                    <Row className="d-flex justify-content-end">
+                        <Col lg="auto">
+                            <Button type="submit">Create</Button>
+                        </Col>
+                    </Row>
+                </Form>
             </Card.Body>
         </Card>
     );
