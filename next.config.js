1a95bcb712277a8616cf1fe9dd1810a4446c1495
Chris Pryer <christophpryer@gmail.com>
2020-07-19 01:01:09 -0400

    updated for /vrp

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
         dev: {
             USER_AUTH_URL: "http://localhost:8080/",
             USER_CRUD_URL: "http://localhost:8081/",
-            VRP_RPC_URL: "http://localhost:5000/api/v0.1/procedure",
+            VRP_RPC_URL: "http://localhost:5000/api/v0.1/vrp",
             GOOGLE_MAPS_KEY: "AIzaSyDCXlrXJeRe3pUbGMXpsh8Z2GVMZA2jVW8"
         },
         prod: {
