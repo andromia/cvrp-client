f23f5c5583a6ffce8fc9c216be3f205657b1cd57
chris pryer <christophpryer@gmail.com>
2020-10-04 20:06:18 -0400

    disabled non-MVP discover cards

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -20,37 +20,38 @@ const Discover = () => {
             <Row className={"d-flex flex-column justify-content-center align-items-center pb-4 mb-2 w-50 mx-auto"}>
                 <Col>
                     <StackCardWithIcon 
-                    title={"Chained Stack Demo"}
-                    sub={"Geocode, Depot, Route (created by @fingafrog)"}
-                    desc={"this is an example of a stack a user creates to chain multiple stacks together"}
-                    link={"/setup/chained"} />
+                    title={"Route"}
+                    sub={"Super Module (created by @cnp)"}
+                    desc={"shipment routing for origin-positioned & geocoded demand .csv data"}
+                    link={"/setup/route"} />
                 </Col>
             </Row>
+            <hr/>
             <Row className={"d-flex flex-column justify-content-center align-items-center pb-4 mb-2 w-50 mx-auto"}>
                 <Col>
                     <StackCardWithIcon 
                     title={"Geocode"}
-                    sub={"Base Module (created by @fingafrog)"}
+                    sub={"Coming soon..."}
                     desc={"geocode zipodes for .csv files"}
-                    link={"/setup/geocode"} />
+                    link={""} />
                 </Col>
             </Row>
             <Row className={"d-flex flex-column justify-content-center align-items-center pb-4 mb-2 w-50 mx-auto"}>
                 <Col>
                     <StackCardWithIcon 
                     title={"Depot"}
-                    sub={"Base Module (created by @fingafrog)"}
+                    sub={"Coming soon..."}
                     desc={"origin location positioning within geocoded demand .csv data"}
-                    link={"/setup/depot"} />
+                    link={""} />
                 </Col>
             </Row>
             <Row className={"d-flex flex-column justify-content-center align-items-center pb-4 mb-2 w-50 mx-auto"}>
                 <Col>
                     <StackCardWithIcon 
-                    title={"Route"}
-                    sub={"Super Module (created by @fingafrog)"}
-                    desc={"shipment routing for origin-positioned & geocoded demand .csv data"}
-                    link={"/setup/route"} />
+                    title={"Chained Stack Demo"}
+                    sub={"Geocode, Depot, Route (Coming soon...)"}
+                    desc={"this is an example of a stack a user creates to chain multiple stacks together"}
+                    link={""} />
                 </Col>
             </Row>
         </Container>
